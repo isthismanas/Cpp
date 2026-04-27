89f636c1d328286cac9c47a6c4fa21fff825cc44
Manas <isthismanas@gmail.com>
2026-04-27 17:57:32 +0930

    Learned the use of my first ever fucntion

--- test.cpp
+++ test.cpp
@@ -2,7 +2,13 @@
 
 using namespace std;
 
-int main() {
+int greet_user() {
   cout << "Hello World!" << endl;
+  return 0;
+}
+
+int main() {
+  int result = greet_user();
+  cout << "Function returned: " << result << endl;
   return 0;
 }
